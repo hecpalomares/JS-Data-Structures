ad783107fd87fbddcfe3310fe1cea43af03cdb43
Hector Palomares <hector.palomares@laureate.net>
2018-04-16 19:03:39 -0500

    Stacks with Classes. Stack with Classes + Symbols. Stacks with WeakMaps.

--- Chapter 3. Stacks/stacks.js
+++ Chapter 3. Stacks/stacks.js
@@ -3,6 +3,7 @@
 *  near the 'base'.
 */
 
+// ES5 Stacks
 function Stack() {
 	// data structure to store the items
 	let items = [];
@@ -29,7 +30,7 @@ function Stack() {
 
 	// returning the number of elements on the stack
 	this.size = function() {
-		return items.length();
+		return items.length;
 	};
 
 	// empties the stack, removes all the elements
@@ -37,4 +38,122 @@ function Stack() {
 		items = [];
 	};
 
+	// output the content of the stack on the console
+	this.print = function() {
+		console.log(items.toString());
+	};
 }
+
+let stack = new Stack();
+console.log(stack.items);			// undefined, not visible to the public
+console.log(stack.isEmpty());	// true
+
+stack.push(4);
+stack.push(8);
+
+console.log(stack.peek());		// 8
+
+stack.push(11);
+
+console.log(stack.size());		// 3
+console.log(stack.isEmpty());	// false
+
+stack.push(15);
+
+stack.pop();
+stack.pop();
+console.log(stack.size());		// 2
+stack.print();								// 4,8
+
+// Stacks with ES6 Class Syntax
+class StackES6 {
+	constructor() {
+		this.items = [];
+	}
+
+	push(element) {
+		this.items.push(element);
+	}
+
+	pop() {
+		this.items.pop();
+	}
+};
+
+let stackES6 = new StackES6();	
+console.log(stackES6.items);	// [], it's public. It doesn't allow to declare private properties since its prototype based. 
+
+// Stacks with ES6 Class Syntax + Symbols()
+let _items = Symbol();
+
+class StackES6PlusSymbol {
+	constructor() {
+		this[_items] = [];
+	}
+
+	push(element) {
+		this[_items].push(element);
+	}
+
+	pop() {
+		this[_items].pop();
+	}
+
+	print() {
+		console.log(this[_items].toString());
+	}
+}
+
+let stackES6X = new StackES6PlusSymbol();
+console.log(stackES6X.items);		// undefined. Provides a flase class private property, not accessible from the outside.
+
+stackES6X.push(5);
+stackES6X.push(3);
+
+stackES6X.print();							// 5, 3
+
+let stackES6X2 = new StackES6PlusSymbol();
+console.log(stackES6X2.items);	// undefined.
+
+stackES6X2.push(6);
+stackES6X2.push(9);
+
+stackES6X2.print();							// 6, 9
+
+// Stacks with ES6 WeakMaps
+let StackWeakMap = (function() {
+	const items = new WeakMap();			// declare items variable as a WeakMap
+
+	class Stack {
+		constructor () {
+			items.set(this, []);					// 'this' (reference to Stack class) as the key of the WeakMap, and the array that represent the stack as its value
+		}
+
+		push(element) {
+			let stack = items.get(this);	// retrieve the value of the items by retrieving the value of the WeakMap, by passing the 'this' as the key
+			stack.push(element);
+		}
+
+		pop() {
+			let stack = items.get(this);
+			let result = stack.pop();
+			return result;
+		}
+
+		print() {
+			let stack = items.get(this);
+			console.log(stack.toString());
+		}
+	}
+
+	return Stack;	// When the constructor of the Stack function is called, return the instance of the 'Stack' class
+
+})();
+
+let stackWeakMap = new StackWeakMap();
+console.log(stackWeakMap.items);	// undefined
+
+stackWeakMap.push(2);
+stackWeakMap.push();
+
+stackWeakMap.print();							// 2, 7
